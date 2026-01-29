1ad99ccec84b57062cd340cb49b73efb3e43aa9f
Ilton Junior <Junior Delacruz>
2026-01-29 15:42:26 -0300

    ac

--- backend/routes/me.routes.js
+++ backend/routes/me.routes.js
@@ -4,6 +4,6 @@ import authMiddleware from '../middleware/auth.js';
 import { getMeuCidadao } from "../controllers/me.controller.js";
 
 const r = Router();
-r.get("/cidadao", authMiddleware(), getMeuCidadao);
+r.get("/cidadao", authMiddleware(['admin']), getMeuCidadao);
 
 export default r;
