4c6c45b92ee7f7582ee34046c11f80b1068188f8
Ilton Junior <Junior Delacruz>
2026-02-02 14:37:41 -0300

    Add

--- backend/server.js
+++ backend/server.js
@@ -124,19 +124,19 @@ app.get('/api', (req, res) => {
 
 
 
-await db.Permission.bulkCreate(
-  [
-    { key: "admin.perms.manage", label: "Gerenciar permissões", group: "Admin" },
+// await db.Permission.bulkCreate(
+//   [
+//     { key: "admin.perms.manage", label: "Gerenciar permissões", group: "Admin" },
 
-    { key: "lancamentos.view_meus", label: "Ver meus lançamentos", group: "Lançamentos" },
-    { key: "lancamentos.view_all", label: "Ver lançamentos geral", group: "Lançamentos" },
-    { key: "lancamentos.create", label: "Criar lançamento", group: "Lançamentos" },
-    { key: "lancamentos.repasse.registrar", label: "Registrar repasse", group: "Lançamentos" },
+//     { key: "lancamentos.view_meus", label: "Ver meus lançamentos", group: "Lançamentos" },
+//     { key: "lancamentos.view_all", label: "Ver lançamentos geral", group: "Lançamentos" },
+//     { key: "lancamentos.create", label: "Criar lançamento", group: "Lançamentos" },
+//     { key: "lancamentos.repasse.registrar", label: "Registrar repasse", group: "Lançamentos" },
 
-    { key: "servicos.manage", label: "Gerenciar serviços", group: "Serviços" },
-  ],
-  { ignoreDuplicates: true }
-);
+//     { key: "servicos.manage", label: "Gerenciar serviços", group: "Serviços" },
+//   ],
+//   { ignoreDuplicates: true }
+// );
 
 
 
